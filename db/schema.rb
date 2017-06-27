6a3c24c68d24d00050414f6a346c2fe4f0f366f4
Sami Hamdan <samihamdan00@gmail.com>
2017-06-27 13:05:47 +0000

    checkpoint28 completd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626231503) do
+ActiveRecord::Schema.define(version: 20170627040956) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170626231503) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,11 +38,18 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.update_attributes!(
-    email: 'samihamdan00@gmail.com',
-    password: 'password'
-    )
+admin = User.create!(
+    name: "Admin User",
+    email: "admin@example.com",
+    password: 'helloworld',
+    role: 'admin'
+)
+
+member = User.create!(
+    name: "Member User",
+    email: "member@example.com",
+    password: "helloworld"
+)
 
 puts "Seed finished"
 puts "#{User.count} users created"
