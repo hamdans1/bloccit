37178a0bb69666605fd8246d886f7cffc58c1611
Sami Hamdan <samihamdan00@gmail.com>
2017-06-30 03:32:04 +0000

    assignment32 completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+    
+    def post_history(user)
+        if user.posts.empty?
+            "#{user.name} has not submitted any posts yet"
+        else
+            render @user.posts
+        end
+    end
+    
+    def comment_history(user)
+        if user.comments.empty?
+            "#{user.name} has not submitted any comments yet"
+        else
+            render @user.comments
+        end
+    end
+    
 end
